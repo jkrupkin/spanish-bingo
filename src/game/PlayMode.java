07ef5f55045c8f26e83a65e60d0ef10c5bacaa21
Rakned <my.spot@verizon.net>
2019-04-29 19:21:39 -0400

    Added in some methods

---
+++ src/game/PlayMode.java
@@ -0,0 +1,22 @@
+package game;
+
+public class PlayMode {
+	//TODO: main program area; main menu setup
+	public static void main(String[] args) {
+		
+	}
+	
+	//TODO: sub-method for practice window
+	private static void practiceMode() {
+		
+	}
+	
+	//TODO: sub-method for bingo game window
+	private static void bingoGame() {
+		
+	}
+	
+	private static String[] getFilenameList() {
+		
+	}
+}
