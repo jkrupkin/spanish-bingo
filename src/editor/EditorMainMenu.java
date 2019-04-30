14322815355a857f273083ab2b6e1f9773c11504
Rakned <my.spot@verizon.net>
2019-04-29 20:46:40 -0400

    relocated code from PlayMode.java to EditorMainMenu.java
    accidentally put Editor UI code in the wrong java file

--- src/editor/EditorMainMenu.java
+++ src/editor/EditorMainMenu.java
@@ -1,8 +1,45 @@
 package editor;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class EditorMainMenu {
+	private static JFrame window;
+	
 	// TODO: Main method for editor program
 	public static void main(String[] args) {
+		window = new JFrame();
 		
+		JPanel mainMenu = new JPanel();
+		mainMenu.setLayout(new BorderLayout());
+		
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout());
+		buttons.add(new JButton("Add New Word(s)"));
+		buttons.add(new JButton("Save Set as Zip File"));
+		mainMenu.add(buttons, BorderLayout.PAGE_END);
+		
+		window.add(mainMenu);
+		
+		/*
+		 * JFrame
+		 * - Center
+		 * - - List of cards currently in system
+		 * - - - Flow Layout
+		 * - - - - Card Name (editable)
+		 * - - - - 
+		 * - Bottom
+		 * - - Flow Layout
+		 * - - - Button("Add New Card(s)")
+		 * - - - Button("Compress to .zip")
+		 */
+		
+		window.setSize(400, 500);
+		window.setVisible(true);
 	}
 }
--- src/game/PlayMode.java
+++ src/game/PlayMode.java
@@ -1,13 +1,9 @@
 package game;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import shared.Word;
 
@@ -19,35 +15,7 @@ public class PlayMode {
 	
 	// TODO: main program area; main menu setup
 	public static void main(String[] args) {
-		window = new JFrame();
 		
-		JPanel mainMenu = new JPanel();
-		mainMenu.setLayout(new BorderLayout());
-		
-		
-		JPanel buttons = new JPanel();
-		buttons.setLayout(new FlowLayout());
-		buttons.add(new JButton("Add New Word(s)"));
-		buttons.add(new JButton("Save Set as Zip File"));
-		mainMenu.add(buttons, BorderLayout.PAGE_END);
-		
-		window.add(mainMenu);
-		
-		/*
-		 * JFrame
-		 * - Center
-		 * - - List of cards currently in system
-		 * - - - Flow Layout
-		 * - - - - Card Name (editable)
-		 * - - - - 
-		 * - Bottom
-		 * - - Flow Layout
-		 * - - - Button("Add New Card(s)")
-		 * - - - Button("Compress to .zip")
-		 */
-		
-		window.setSize(400, 500);
-		window.setVisible(true);
 	}
 	
 	private class mainMenu extends JFrame {
