404d11a74db0ed2b3aa7646be12c61b333e78872
Rakned <my.spot@verizon.net>
2019-04-29 20:37:15 -0400

    Update PlayMode.java
    Added start of editor menu UI

--- src/game/PlayMode.java
+++ src/game/PlayMode.java
@@ -1,18 +1,51 @@
 package game;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 // All "main operation" code should be stored in this class
 // A static class for main game code
 public class PlayMode {
+	private static JFrame window;
 	private static List<Word> wordList;
 	
 	// TODO: main program area; main menu setup
 	public static void main(String[] args) {
+		window = new JFrame();
 		
+		JPanel mainMenu = new JPanel();
+		mainMenu.setLayout(new BorderLayout());
+		
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout());
+		buttons.add(new JButton("Add New Word(s)"));
+		buttons.add(new JButton("Save Set as Zip File"));
+		mainMenu.add(buttons, BorderLayout.PAGE_END);
+		
+		window.add(mainMenu);
+		
+		/*
+		 * JFrame
+		 * - Center
+		 * - - List of cards currently in system
+		 * - - - Flow Layout
+		 * - - - - Card Name (editable)
+		 * - - - - 
+		 * - Bottom
+		 * - - Flow Layout
+		 * - - - Button("Add New Card(s)")
+		 * - - - Button("Compress to .zip")
+		 */
+		
+		window.setSize(400, 500);
+		window.setVisible(true);
 	}
 	
 	private class mainMenu extends JFrame {
