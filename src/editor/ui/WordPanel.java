b70600dd70192c419d979ac6e99dad4ad0ac96ab
Rakned <my.spot@verizon.net>
2019-05-01 15:59:47 -0400

    Update WordPanel.java

--- src/editor/ui/WordPanel.java
+++ src/editor/ui/WordPanel.java
@@ -3,14 +3,19 @@ package editor.ui;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class WordPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
 	WordPanel(String word, String imagePath, String soundPath) {
 		super();
 		setLayout(new FlowLayout());
 		
+		// TODO make interface tools actually functional
+		this.add(new JCheckBox());
 		this.add(new JTextField(word));
 		this.add(new JTextField(imagePath));
 		this.add(new JTextField(soundPath));
