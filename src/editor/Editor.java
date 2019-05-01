068e1038bd7e8356b0ede3ab07963f2c087a4c87
Rakned <my.spot@verizon.net>
2019-05-01 10:59:59 -0400

    Update Editor.java

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -20,38 +20,4 @@ public class Editor {
 	public static void main(String[] args) {
 		
 	}
-	
-	private static class AddWordListener implements ActionListener {
-		JPanel target;
-		
-		AddWordListener(JPanel target) {
-			this.target = target;
-		}
-		
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			genNewWord("debug");
-			target.revalidate();
-		}
-		
-		private void genNewWord(String filePath) {
-			JPanel panel = new JPanel();
-			panel.setLayout(new FlowLayout());
-			JTextField t = new JTextField();
-			t.setText(filePath);
-			panel.add(t);
-			JButton b = new JButton("SND");
-			panel.add(b);
-			b = new JButton("DEL");
-			panel.add(b);
-			target.add(panel);
-		}
-	}
-	
-	private static class WordPanel extends JPanel {
-		WordPanel() {
-			super();
-			setLayout(new FlowLayout());
-		}
-	}
 }
