19a102c85ea24f998b9231fa2292d5a2f3f11d3d
jkrupkin <jpkrupkin@gmail.com>
2019-05-02 18:26:42 -0400

    Added method to convert to JSON

--- src/shared/FileHandler.java
+++ src/shared/FileHandler.java
@@ -1,13 +1,30 @@
 package shared;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.zip.*;
+import org.json.*;
 public class FileHandler {
 
+	//Creates .zip file from given ArrayList
 	public void writeVocab(ArrayList<Word> words) {
 		
 	}
-	public ArrayList<Word> readVocab()
+	//Creates an ArrayList of Words with given .zip file
+	public ArrayList<Word> readVocab(File zip)
 	{
 		
 		return new ArrayList<Word>(); //TODO: Change return variable
+	}
+	private String createJSON(ArrayList<Word> words) {
+		ArrayList<String> word = new ArrayList<String>();
+		ArrayList<String> imagePath = new ArrayList<String>();
+		ArrayList<String> audioPath = new ArrayList<String>();
+		for(Word w : words) {
+			word.add(w.getWord());
+			imagePath.add(w.getPic().getPath());
+			audioPath.add(w.getAudioClip().getPath());
+			
+		}
+		return "";
 	}
 }
