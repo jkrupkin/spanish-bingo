4302ccbe05ba0594afc9b636a1f3fc875b89e5ca
Rakned <my.spot@verizon.net>
2019-05-02 16:59:04 -0400

    Update EditorUI.java
    edits to UI backend to make things easier to edit and control later on

--- src/editor/ui/EditorUI.java
+++ src/editor/ui/EditorUI.java
@@ -12,78 +12,70 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class EditorUI extends JFrame {
-	/**
-	 * default serial version ID
-	 * aka: JAVA STOP SHOUTING AT ME, I KNOW WHAT IM DOING MOOOOOOM
-	 */
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 400, HEIGHT = 500;
+	
+	ScrollPane wordScroller;
+	JPanel wordPanel, buttons;
+	JButton newWordButton, saveSetButton;
+	ButtonListener listener;
 
 	public EditorUI() {
+		// initial setup
 		super();
-		
 		setLayout(new BorderLayout());
 		
-		ScrollPane wordScroller = new ScrollPane();
+		// central scrolling area
+		wordScroller = new ScrollPane();
 		this.add(wordScroller, BorderLayout.CENTER);
 		
-		JPanel wordPanel = new JPanel();
+		wordPanel = new JPanel();
 		wordPanel.setLayout(new BoxLayout(wordPanel, BoxLayout.PAGE_AXIS));
 		wordScroller.add(wordPanel);
 		
-		JPanel buttons = new JPanel();
+		// button panel setup
+		buttons = new JPanel();
 		buttons.setLayout(new FlowLayout());
 		this.add(buttons, BorderLayout.PAGE_END);
 		
-		JButton b = new JButton("Add New Word");
-		b.addActionListener(new AddWordListener(wordPanel));
-		buttons.add(b);
+		listener = new ButtonListener();
 		
-		b = new JButton("Remove selected words");
-		b.addActionListener(new DropWordListener());
-		buttons.add(b);
+		newWordButton = new JButton("Add New Word");
+		newWordButton.addActionListener(listener);
+		buttons.add(newWordButton);
 		
-		// TODO at current width + layout, this button gets cut off
-		// 		find a way to set things up so it isn't
-		b = new JButton("Save Set as Zip File");
-		b.addActionListener(new ZipWordListener());
-		buttons.add(b);
+		saveSetButton = new JButton("Save Set as Zip File");
+		saveSetButton.addActionListener(listener);
+		buttons.add(saveSetButton);
 		
+		
+		// final initialization
 		this.setSize(WIDTH, HEIGHT);
 		this.setVisible(true);
 	}
 	
-	private static class AddWordListener implements ActionListener {
-		JPanel target;
+	private class ButtonListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent event)
+		{
+			JButton b = (JButton) event.getSource();
+			if (b == newWordButton)
+			{
+				// TODO Code for "properly" adding a new word to the set
+				// will probably use a popup dialogue window of some sort
+				// current version is just debug code
+				
+				add(new WordPanel("word", "imageLocation", "soundLocation"));
+				revalidate();
+			}
+			else if (b == saveSetButton)
+			{
+				// TODO code for saving all files to a compressed .zip folder
+				
+			}
+		}
 		
-		AddWordListener(JPanel target) {
-			this.target = target;
-		}
-		
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Code for "properly" adding a new word to the set
-			// will probably use a popup dialogue window of some sort
-			// current version is just debug code
-			
-			target.add(new WordPanel("word", "imageLocation", "soundLocation"));
-			target.revalidate();
-		}
-	}
-	
-	// TODO implement word removal
-	private static class DropWordListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			
-		}
-	}
-	
-	// TODO implement "zip contents to file" functionality
-	private static class ZipWordListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			
-		}
 	}
 }
