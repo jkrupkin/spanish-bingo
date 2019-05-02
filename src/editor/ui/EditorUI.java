73fe7d984dcb425705defd726fd4f9abbb497874
Rakned <my.spot@verizon.net>
2019-05-02 17:20:49 -0400

    Update EditorUI.java
    Removed extraneous additional class, designated the UI itself as the action listener

--- src/editor/ui/EditorUI.java
+++ src/editor/ui/EditorUI.java
@@ -11,14 +11,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class EditorUI extends JFrame {
+public class EditorUI extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 400, HEIGHT = 500;
 	
 	ScrollPane wordScroller;
 	JPanel wordPanel, buttons;
 	JButton newWordButton, saveSetButton;
-	ButtonListener listener;
 
 	public EditorUI() {
 		// initial setup
@@ -38,14 +37,12 @@ public class EditorUI extends JFrame {
 		buttons.setLayout(new FlowLayout());
 		this.add(buttons, BorderLayout.PAGE_END);
 		
-		listener = new ButtonListener();
-		
 		newWordButton = new JButton("Add New Word");
-		newWordButton.addActionListener(listener);
+		newWordButton.addActionListener(this);
 		buttons.add(newWordButton);
 		
 		saveSetButton = new JButton("Save Set as Zip File");
-		saveSetButton.addActionListener(listener);
+		saveSetButton.addActionListener(this);
 		buttons.add(saveSetButton);
 		
 		
@@ -54,28 +51,23 @@ public class EditorUI extends JFrame {
 		this.setVisible(true);
 	}
 	
-	private class ButtonListener implements ActionListener
+	@Override
+	public void actionPerformed(ActionEvent event)
 	{
-
-		@Override
-		public void actionPerformed(ActionEvent event)
+		JButton b = (JButton) event.getSource();
+		if (b == newWordButton)
 		{
-			JButton b = (JButton) event.getSource();
-			if (b == newWordButton)
-			{
-				// TODO Code for "properly" adding a new word to the set
-				// will probably use a popup dialogue window of some sort
-				// current version is just debug code
-				
-				add(new WordPanel("word", "imageLocation", "soundLocation"));
-				revalidate();
-			}
-			else if (b == saveSetButton)
-			{
-				// TODO code for saving all files to a compressed .zip folder
-				
-			}
+			// TODO Code for "properly" adding a new word to the set
+			// will probably use a popup dialogue window of some sort
+			// current version is just debug code
+			
+			wordPanel.add(new WordPanel("word", "imageLocation", "soundLocation"));
+			wordPanel.revalidate();
 		}
-		
+		else if (b == saveSetButton)
+		{
+			// TODO code for saving all files to a compressed .zip folder
+			
+		}
 	}
 }
