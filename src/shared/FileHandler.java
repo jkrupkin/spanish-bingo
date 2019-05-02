30b4db4077fff39c5788e115b6d67289c78f6b78
jkrupkin <jpkrupkin@gmail.com>
2019-05-02 17:38:27 -0400

    Added FileHandler

---
+++ src/shared/FileHandler.java
@@ -0,0 +1,13 @@
+package shared;
+import java.util.ArrayList;
+public class FileHandler {
+
+	public void writeVocab(ArrayList<Word> words) {
+		
+	}
+	public ArrayList<Word> readVocab()
+	{
+		
+		return new ArrayList<Word>(); //TODO: Change return variable
+	}
+}
