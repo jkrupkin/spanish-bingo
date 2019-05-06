780a17b2d62e632905bb4f96c079e900e9633640
jkrupkin <jpkrupkin@gmail.com>
2019-05-05 21:17:03 -0400

    Created method to write zip file

--- src/shared/FileHandler.java
+++ src/shared/FileHandler.java
@@ -1,7 +1,12 @@
 package shared;
-import java.io.File;
 import java.io.BufferedWriter;
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.zip.*;
 import org.json.JSONArray;
@@ -9,13 +14,53 @@ import org.json.JSONObject;
 public class FileHandler {
 
 	// TODO Creates .zip file from given ArrayList
-	public void writeVocab(ArrayList<Word> words) {
+	public void writeVocab(ArrayList<Word> words, String path) throws IOException {
+		///Create the JSON String and directory Structure for .zip file
 		String json = createJSON(words);
 		System.out.println(json);
 		File audioDir = new File("audio");
 		File imageDir = new File("images");
+		File wordsJSON = new File("words.json");
 		audioDir.mkdir();
-		imageDir.mkdir();
+		imageDir.mkdir();		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(wordsJSON));
+		bw.write(json);
+		bw.close();
+		
+		FileOutputStream fos = new FileOutputStream(path);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		for (Word w : words) {
+			Path newImagePath = Paths.get("images/" + w.getPic().getName());
+			Path originalImagePath = w.getPic().toPath();
+			
+			Path newAudioPath = Paths.get("audio/" + w.getAudioClip().getName());
+			Path originalAudioPath = w.getPic().toPath();
+			Files.copy(originalImagePath, newImagePath, StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(originalAudioPath, newAudioPath, StandardCopyOption.REPLACE_EXISTING);
+			
+			toZip("images/" + w.getPic().getName(), zos);
+			toZip("audio/" + w.getAudioClip().getName(), zos);
+
+			
+		}
+		toZip(wordsJSON.getPath(), zos);
+		zos.close();
+		fos.close();
+
+		
+	}
+	private void toZip(String f, ZipOutputStream zos ) throws FileNotFoundException, IOException {
+		FileInputStream fis = new FileInputStream(f);
+		ZipEntry ze = new ZipEntry(f);
+		zos.putNextEntry(ze);
+		
+		byte[] bytes = new byte[1024];
+		int length;
+		while((length = fis.read(bytes)) >= 0)
+			zos.write(bytes,0,length);
+		zos.closeEntry();
+		fis.close();
+		
 	}
 	// TODO Creates an ArrayList of Words from given .zip file
 	public ArrayList<Word> readVocab(File zip)
