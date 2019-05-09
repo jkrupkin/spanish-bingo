97d39904a9291abc8b10361391954976e7bdf5ca
Rakned <my.spot@verizon.net>
2019-05-08 22:30:36 -0400

    Update Editor.java
    we should probably get rid of this + shift the static method to EditorUI, since there isn't much else going on with it (the UI interface/callbacks control most things)

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -1,18 +1,5 @@
 package editor;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.ScrollPane;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 import editor.ui.EditorUI;
 
 public class Editor {
