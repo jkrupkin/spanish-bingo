9e01c4ff43f116f8a41c1aef924acd1fe96fdf1f
Rakned <my.spot@verizon.net>
2019-05-10 15:09:22 -0400

    relocated editor main method

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -3,10 +3,8 @@ package editor;
 import editor.ui.EditorUI;
 
 public class Editor {
-	private static EditorUI ui;
-	
 	// entry method for editor program
 	public static void main(String[] args) {
-		ui = new EditorUI();
+		new EditorUI();
 	}
 }
