09d1768aacfbc98785fc027779781589a8750725
Rakned <my.spot@verizon.net>
2019-05-10 15:09:29 -0400

    Revert "relocated editor main method"
    This reverts commit 9e01c4ff43f116f8a41c1aef924acd1fe96fdf1f.

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -3,8 +3,10 @@ package editor;
 import editor.ui.EditorUI;
 
 public class Editor {
+	private static EditorUI ui;
+	
 	// entry method for editor program
 	public static void main(String[] args) {
-		new EditorUI();
+		ui = new EditorUI();
 	}
 }
