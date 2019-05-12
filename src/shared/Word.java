c64c43acd85f3b8069103f3ad090b06442155e64
jkrupkin <jpkrupkin@gmail.com>
2019-05-12 16:43:38 -0400

    Created new constructors
    Class now contains raw image data
    Added method to extract files from zip that match with the word

--- src/shared/Word.java
+++ src/shared/Word.java
@@ -2,34 +2,65 @@ package shared;
 
 import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.imageio.ImageIO;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONPointer;
 
 public class Word {
 	private String word;
 	private File audio, pic;
-	private Image image;
-
-	public Word(String word, String picturePath, String audioPath) {
+	private ZipFile zip;
+	private Image imageData;
+	
+//Constructor for reading and writing
+	public Word(String word, String picturePath, String audioPath) throws IOException {
 		this.word = word;
 		pic = new File(picturePath);
 		audio = new File(audioPath);
+		imageData = ImageIO.read(pic);
 	}
-	
-	public Word(String word, File pictureFile, File audioFile) {
+//Constructor for read-only usage from a zip
+	public Word(String word, String zipStr) throws IOException {
 		this.word = word;
-		pic = pictureFile;
-		audio = audioFile;
+		zip = new ZipFile(zipStr);
+		JSONArray wordsJSON = FileHandler.readJSON(zip);
+		for(int i=0; i < wordsJSON.length(); i++) {
+			JSONObject wordJSON = wordsJSON.getJSONObject(i);
+			String currentWord = wordJSON.getString("word");
+			if(currentWord.equals(word))
+				break;
+			
+		}
+		
+		
 	}
 	
-	public File getPic() {
+	protected File getPic() {
 		return pic;
 	}
 
-	public File getAudioClip() {
+	protected File getAudioClip() {
 		return audio;
+	}
+	public Image getImage() {
+		return imageData;
 	}
 
 	public String getWord() {
 		return word;
+	}
+	private InputStream extractFile(String file) throws IOException {
+		ZipEntry ze = zip.getEntry(file);
+		InputStream is = zip.getInputStream(ze);
+		return is;
+		
 	}
 	
 	
