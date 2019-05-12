eb2c949e3e842fe712c6669218827b2a6b922f9e
Rakned <my.spot@verizon.net>
2019-05-12 17:23:19 -0400

    Update MenuUI.java

--- src/game/ui/MenuUI.java
+++ src/game/ui/MenuUI.java
@@ -2,11 +2,8 @@ package game.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.JPanel;
-
-import shared.Word;
 
 @SuppressWarnings("serial")
 public class MenuUI extends JPanel implements ActionListener {
