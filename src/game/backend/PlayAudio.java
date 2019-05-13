34af69b85e7ecd6613e7299283e7cd8ce87e1130
jkrupkin <jpkrupkin@gmail.com>
2019-05-13 19:08:23 -0400

    Added boolean to detect whether sound is playing

--- src/game/backend/PlayAudio.java
+++ src/game/backend/PlayAudio.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javax.sound.sampled.*;
 import shared.Word;
 public class PlayAudio {
-
+private static boolean isPlaying;
 	public static void play(Word w) throws LineUnavailableException, IOException {
 			System.out.println("Playing audio");
 			Clip c = AudioSystem.getClip();
@@ -14,6 +14,7 @@ public class PlayAudio {
 				@Override
 				public void update(LineEvent e) {
 					if(e.getType() == LineEvent.Type.STOP) {
+						isPlaying = false;
 						System.out.println("Playback stopped");
 						c.close();
 						try {
@@ -25,12 +26,18 @@ public class PlayAudio {
 						
 
 					}
+					if(e.getType() == LineEvent.Type.START) {
+						isPlaying = true;
+					}
 					
 				}
 			};
 			c.addLineListener(l);
 			c.start();
 		}
+	public static boolean isPlaying() {
+		return isPlaying;
+	}
 
 	}
 
