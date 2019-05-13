deb7c00be1044807254bf255b36df72420873593
Rakned <my.spot@verizon.net>
2019-05-13 13:18:26 -0400

    Update EditMode.java

--- src/editor/EditMode.java
+++ src/editor/EditMode.java
@@ -2,7 +2,7 @@ package editor;
 
 import editor.ui.EditorUI;
 
-public class Editor {
+public class EditMode {
 	// entry method for editor program
 	public static void main(String[] args) {
 		new EditorUI();
